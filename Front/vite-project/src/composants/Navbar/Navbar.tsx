f53f335df50085ffd778bc95fb69409938b9dc7f
unknown <dorine.journet@laposte.net>
2024-07-25 09:36:25 +0200

    FRONT react

---
+++ Front/vite-project/src/composants/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+export function navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/list"> Liste des voitures </a>
+                </li>
+            </ul>
+        </nav>
+    )
+  }
